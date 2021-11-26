b42b4dc882887f27e62a62b003a28cdfa54bc6a6
原罪 <531362022@qq.com>
2021-11-26 16:13:00 +0800

    fix: use getStorageSync for miniprogram

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,48 @@
 import type { StorageEngine } from 'foca';
-import Taro from '@tarojs/taro';
+import {
+  getStorage,
+  getStorageSync,
+  setStorage,
+  removeStorage,
+  clearStorage,
+} from '@tarojs/taro';
+
+/**
+ * 持久化必须在小程序onLoad之前完成，否则页面报错
+ * @link https://github.com/NervJS/taro/issues/6548#issuecomment-717896033
+ */
+const isMiniProgram =
+  process.env.TARO_ENV !== 'rn' &&
+  process.env.TARO_ENV !== 'h5' &&
+  process.env.TARO_ENV !== 'quickapp';
 
 export const taroStorage: StorageEngine = {
   getItem(key) {
-    return Taro.getStorage({ key })
-      .then(({ data }) => {
+    const promise = isMiniProgram
+      ? Promise.resolve().then(() => getStorageSync<string>(key))
+      : getStorage<string>({ key }).then(({ data }) => data);
+
+    return promise
+      .then((data) => {
         return typeof data === 'string' ? data : null;
       })
       .catch(() => {
-        // 找不到key时，小程序会抛出异常: getStorage:fail data not found
+        // 使用getStorage()找不到key时，Taro会抛出异常: getStorage:fail data not found
         return null;
       });
   },
   setItem(key, value) {
-    return Taro.setStorage({
+    return setStorage({
       key,
       data: value,
     });
   },
   removeItem(key) {
-    return Taro.removeStorage({
+    return removeStorage({
       key,
     });
   },
   clear() {
-    return Taro.clearStorage();
+    return clearStorage();
   },
 };
