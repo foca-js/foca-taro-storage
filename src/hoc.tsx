0aeca61539cb406f24a4e706f872391bbebe0c91
geekact <fanwenhua1990@gmail.com>
2022-05-13 01:24:55 +0800

    refactor: reduce size

--- src/hoc.tsx
+++ src/hoc.tsx
@@ -5,12 +5,13 @@ import React, { ComponentType, useEffect, useState, FC } from 'react';
 // 路由入口没有ref的需求，所以不需要增加forwardRef
 export function persistInterceptor<T>(EntryComponent: ComponentType<T>) {
   const HOC: FC<T> = (props) => {
-    const [ready, setReady] = useState(store.isReady);
+    const state = useState(store.isReady);
+    const ready = state[0];
 
     useEffect(() => {
       ready ||
         store.onInitialized().then(() => {
-          setReady(true);
+          state[1](true);
         });
     }, []);
 
