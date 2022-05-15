3f77208f1cc419737b2a50b1bf3d1bf2b2dfcddd
geekact <fanwenhua1990@gmail.com>
2022-05-15 15:14:23 +0800

    chore: minimum taro version is 3.0.12 now

--- src/storage.ts
+++ src/storage.ts
@@ -8,6 +8,7 @@ import {
 
 export const taroStorage: StorageEngine = {
   getItem(key) {
+    // getStorage的泛型在taro@3.0.12才加上去，所以在peerDependencies中限定了taro的小版本
     return getStorage<string>({ key }).then(
       ({ data }) => {
         return typeof data === 'string' ? data : null;
